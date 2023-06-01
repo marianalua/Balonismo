d38966ee73f7101467b96aacd577a3618b6d9c0a
marianalua <yuzukisilva3@gmail.com>
2023-06-01 11:02:13 -0300

    inicial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        public static void main(String[] args) {
+
+            Pessoa Pessoa = new Pessoa();
+            Pessoa p1;
+
+
+        }
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,18 @@
+public class Pessoa {
+    public static void main(String[] args) {
+
+        //Variáveis
+
+        Pessoa p1 = new Pessoa();
+        Pessoa p2 = new Pessoa();
+        Pessoa piloto = new Pessoa();
+
+        String Pessoa p1{
+            String nome;
+            String cpf;
+            String telefone;
+            String rg;
+            String assinatura;
+        }
+    }
+}
